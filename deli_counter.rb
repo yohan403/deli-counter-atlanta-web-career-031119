a2e8ca0f4c06742af370ce7580b2bd10f3238c6f
John Cho <johnycho16@gmail.com>
2019-03-02 07:39:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def line(list)
+  if list.size == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently:"
+    list.size.times do |i|
+      string = string + " #{i.to_i + 1}. #{list[i]}"
+    end
+    puts string
+  end
+end
+
+
+def take_a_number(list,person)
+  if list.size == 0
+    list.push(person)
+  else
+    list.push(person)
+  end
+end
